6f1d3f737eb843d4ac04cfae2d21140253475da4
NumenMirus <michaelbastauni@gmail.com>
2022-04-23 08:18:04 +0200

    added login form

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from crypt import methods
 from flask import *
 
 app = Flask(__name__)
+
+# SQLite supporta database transienti in RAM (echo attiva il logging)
+
+#TODO: oscuare credenziali portandole duore dalla cartella pubblica
+#engine = create_engine('postgresql://postgres:trolese@localhost:5432/progetto', echo = True)
 
 @app.route('/')
 def index():
@@ -10,7 +16,7 @@ def index():
 def profile():
     return render_template("profile.html")
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
