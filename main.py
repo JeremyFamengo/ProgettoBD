e723360294c1e0ae66482ab0e34a1c03c6adab4c
NumenMirus <michaelbastauni@gmail.com>
2022-04-23 12:23:17 +0200

    added login logic

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from crypt import methods
 from flask import *
+import os
 
 app = Flask(__name__)
+
+app.secret_key="AS7wvAhaKu4yFyVuPaTasCUDY6mg8c3RmjMFAAtQCfAxrUZxt5xZbTbVy8rHYagkAYG52jrVSz6aMBDPQt6bVLnPzd7ZBbCwAZnazwKkuYNvnKMVSqppmnvSV8xrwJZMXhPdQY6bhgHUjxx3cwHZkB66v4uYZWmdBNaLuDrnFZFgJS58KnSnPuQa2zQYjzqCZEZzz3gscmZvNCfhaRSFaM4AKu2UaHcW9K9Cqnf5pFLvBPTFmbAJCsuVEHPvKNSL"
 
 # SQLite supporta database transienti in RAM (echo attiva il logging)
 
@@ -14,15 +17,34 @@ def index():
 
 @app.route('/profile')
 def profile():
-    return render_template("profile.html")
+    if 'user' in session:
+        user = session['user']
+        return render_template("profile.html", user=user)
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        user = request.form['user']
+        password = request.form['pass']
+        if user != '' and password != '':
+            session['user'] = user
+            session['password'] = password
+            return redirect(url_for('profile'))
+        else:
+            return render_template("login.html")
+    else:
+        return render_template("login.html")
 
 @app.route('/info')
 def info():
     return render_template("info.html")
+
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    session.pop('password')
 
 if __name__ == "__main__":
     app.run(debug=True)
