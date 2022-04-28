6c171cfd3ccabe4da522928448580686d65cece6
NumenMirus <michaelbastauni@gmail.com>
2022-04-28 17:55:34 +0200

    added more classes
    Co-authored-by: JeremyFamengo <JeremyFamengo@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from faulthandler import dump_traceback_later
 from flask import *
 from flask_sqlalchemy import *
 from flask_login import UserMixin, current_user, login_user, LoginManager, login_required, logout_user
+from sqlalchemy import PrimaryKeyConstraint
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, DateField, TextAreaField
@@ -109,6 +111,111 @@ class ArtistForm(FlaskForm):
     info = TextAreaField("Tell us what makes you special!", validators=(DataRequired(), Length(min=100)))
     submit = SubmitField("I'm ready!")
 
+class Richieste_diventa_artista(db.Model):
+    __tablename__ = "richieste_diventa_artista"
+    nome_arte = db.Column(db.String(40))
+    id  = db.Column(db.Integer, primary_key=True)
+    motivazione = db.Column(db.String(40))
+    stato_richiesta = db.Column(db.String(20))
+    id_utente = db.Column(db.String(20))
+    
+    psw = db.Column(db.String(128))
+
+    def __init__(self, id, nome_arte, motivazione, stato_richiesta, id_utente):
+        self.id = id
+        self.nome_arte = nome_arte
+        self.motivazione = motivazione
+        self.stato_richiesta = stato_richiesta
+        self.id_utente = id_utente
+
+
+    def debug(self):
+        print("\n---------[DEBUG]---------\n")
+        print(self.id)
+        print(self.nome_arte)
+        print(self.motivazione)
+        print(self.stato_richiesta)
+        print(self.id_utente)
+        print("\n-------------------------\n")
+    
+class Artista(db.Model):
+    __tablename__ = "artisti"
+    id_artista = db.Column(db.Integer, primary_key = True)
+    nome_arte = db.Column(db.String)
+
+    def __init__(self, id_artista, nome_arte):
+        self.id_artista = id_artista
+        self.nome_arte = nome_arte
+
+    def debug(self):
+        print("\n---------[DEBUG]---------\n")
+        print(self.id_artista)
+        print(self.nome_arte)
+        print("\n-------------------------\n")
+
+class Artisti_album(db.Model):
+    __tablename__ = "artisti_album"
+    id_artista = db.Column(db.Integer, primary_key = True)
+    id_album = db.Column(db.Integer, primary_key = True)
+    
+    def __init__(self, id_artista, id_album):
+        self.id_album = id_album
+        self.id_artista = id_artista
+
+class Album(db.Model):
+    __tablename__ = 'album'
+    id_album = db.Column(db.Integer, primary_key = True)
+    titolo = db.Column(db.String)
+    anno = db.Column(db.Date)
+
+    def __init__(self, id_album, titolo, anno):
+        self.id_album = id_album
+        self.titolo = titolo
+        self.anno = anno
+
+class Album_canzoni(db.Model):
+    __tablename__ = 'album_canzoni'
+    id_album = db.Column(db.Integer, primary_key = True)
+    id_canzoni = db.Column(db.Integer, primary_key = True)
+
+    def __intit__(self, id_album, id_canzoni):
+        self.id_album = id_album
+        self.id_canzoni = id_canzoni
+
+class Canzoni(db.Model):
+    __tablename__ = 'canzoni'
+    id_canzone = db.Column(db.Integer, primary_key = True)
+    titolo = db.Column(db.String)
+    durata = db.Column(db.Integer)
+    anno = db.Column(db.Date)
+    id_genere_musicale = db.Column(db.Integer)
+    file = db.Column(db.LargeBinary)
+
+    def __intit__(self, id_canzone, titolo, durata, anno, id_genere_musicale, file):
+        self.id_canzone = id_canzone
+        self.titolo = titolo
+        self.durata = durata
+        self.anno = anno
+        self.id_genere_musicale = id_genere_musicale
+        self.file = file
+
+class Generi_Musicali(db.Model):
+    __tablename__ = 'generi_musicali'
+    id_genere_musicale = db.Column(db.Integer, primary_key = True)
+    nome = db.Column(db.String)
+
+    def __init__(self, id_genere_musicale, nome):
+        self.id_genere_musicale = id_genere_musicale
+        self.nome = nome      
+
+class Playlist(db.Model):
+    __tablename__  = 'playlist'
+    id_playlist = db.Column(db.Integer, primary_key = True)
+    titolo = db.Column(db.String)
+    id_utente = db.Column(db.Integer)
+    id_playlist_canzoni = db.Column(db.Integer)
+
+    
 
 #######################################################
 # ROUTES
@@ -119,7 +226,6 @@ class ArtistForm(FlaskForm):
 def home():
     return render_template("index.html")
 
-#login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -209,14 +315,28 @@ def profileinfo():
 @login_required
 def artist():
     artist = False
+    nome_arte = None 
+    motivazione = None
+    stato_richiesta = None
+    richiesta_effettuata = False
+    richieste_diventa_artista = Richieste_diventa_artista.query.filter_by(id_utente = current_user.cf).first()
 
-    if current_user.id_artista != None:
-        artist=True
-        nome_arte = 'Singer2000' # recuperare da database il nome d'arte
-        return render_template('artist.html', artist = artist, nome_arte = nome_arte)
+    # #TODO: rimuovere e nel caso fosse già un arista restituire il nome, altrimenti il nome_arte sarà null (vuoto)
+    # if current_user.id_artista != None:  
+    #     # recuperare da database i dati dalla tabella richieste_diventa_artista
+    #     artista=Artista.query.get(current_user.id_artista).first()
+    #     artist=True
+    #     richiesta_effettuata=True
+    #     nome_arte=artista.nome_arte
+    # elif richieste_diventa_artista.id != None :
+    #     richiesta_effettuata = True
+    #     nome_arte = richieste_diventa_artista.nome_arte
+    #     motivazione = richieste_diventa_artista.motivazione
+    #     stato_richiesta = richieste_diventa_artista.stato_richiesta
 
-    form = ArtistForm()    
-    return render_template('artist.html', artist = artist, form = form)
+    form = ArtistForm()
+        
+    return render_template('artist.html', artist = artist, richiesta_effettuata=richiesta_effettuata, form=form)
 
 #######################################################
 # FUNCTIONS
