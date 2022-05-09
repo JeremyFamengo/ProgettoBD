b209cccb83a7fa9fa1d835cc0d05a8e525719e0f
NumenMirus <michaelbastauni@gmail.com>
2022-05-09 15:26:32 +0200

    added error 404 handling

--- main.py
+++ main.py
@@ -601,6 +601,11 @@ def player():
     
     return render_template("player.html", canzone=canzone, artista=artista, riservato=riservato, genere=genere, descrizione=descrizione)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
 #######################################################   
 # FUNCTIONS
 #######################################################
